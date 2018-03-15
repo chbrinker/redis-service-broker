6b75fde8faa61e6a92839bd26afc42f483df6107
Johannes Hiemer <jhiemer@evoila.de>
2018-03-15 14:46:45 +0100

    Updating dependencies and smaller code changes

--- osb-service/src/main/java/de/evoila/cf/cpi/bosh/RedisDeploymentManager.java
+++ osb-service/src/main/java/de/evoila/cf/cpi/bosh/RedisDeploymentManager.java
@@ -14,10 +14,7 @@ public class RedisDeploymentManager extends DeploymentManager {
 
     public static final String INSTANCE_GROUP = "redis";
     public static final String REDIS_PASSWORD = "password";
-    public static final String NODES = "nodes";
     public static final String PORT = "port";
-    public static final String VM_TYPE = "vm_type";
-    public static final String DISK_TYPE = "disk_type";
 
     RedisDeploymentManager(BoshProperties boshProperties){
         super(boshProperties);
@@ -39,17 +36,6 @@ public class RedisDeploymentManager extends DeploymentManager {
         serviceInstance.setPassword(randomPassword);
         redis.put(REDIS_PASSWORD, randomPassword);
 
-        if(properties.containsKey(NODES)){
-            manifest.getInstance_groups().get(0).setInstances((Integer) properties.get(NODES));
-        }
-
-        if(properties.containsKey(VM_TYPE)){
-            manifest.getInstance_groups().get(0).setVm_type((String) properties.get(VM_TYPE));
-        }
-
-        if(properties.containsKey(DISK_TYPE)){
-            manifest.getInstance_groups().get(0).setVm_type((String) properties.get(DISK_TYPE));
-        }
-
+        this.updateInstanceGroupConfiguration(manifest, plan);
     }
 }
