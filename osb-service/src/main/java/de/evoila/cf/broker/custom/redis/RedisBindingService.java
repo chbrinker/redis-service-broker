96ed861c0d6c61233608277039f516d94b281e87
Patrick Weber <pweber@evoila.de>
2019-03-20 09:54:05 +0100

    add missing import

--- osb-service/src/main/java/de/evoila/cf/broker/custom/redis/RedisBindingService.java
+++ osb-service/src/main/java/de/evoila/cf/broker/custom/redis/RedisBindingService.java
@@ -16,6 +16,7 @@ import de.evoila.cf.cpi.bosh.CredentialConstants;
 import de.evoila.cf.security.credentials.CredentialStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
